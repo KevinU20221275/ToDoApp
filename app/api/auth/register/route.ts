2a003145555d97321e9da1564b3d485742c1c51c
KevinU20221275 <U20221275@univo.edu.sv>
2025-02-21 15:20:11 -0600

    fixed typescript error

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -21,6 +21,7 @@ export async function POST(request:NextRequest){
         }
     
     } catch (error) {
+        console.error(error);
         return NextResponse.json({error: "An error occurred"},{status:500})
     }
 }
