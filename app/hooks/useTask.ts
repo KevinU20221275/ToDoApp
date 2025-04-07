a017e945a3cecaec9356fc830dadc8246533917b
KevinU20221275 <U20221275@univo.edu.sv>
2025-04-07 12:26:03 -0600

    implement tasks micro service

--- app/hooks/useTask.ts
+++ app/hooks/useTask.ts
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react"
 import { Task } from "../lib/definitions"
+import { addTask, deleteTask, fetchTasks, updateTask } from "../services/tasks"
+import toast from "react-hot-toast"
 
 export function useTask(){
     const [showModal, setShowModal] = useState<boolean>(false)
     const [tasks, setTasks] = useState<Task[]>([])
     const [taskEdit, setTaskEdit] = useState<Task | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
     
     useEffect(() => {
-        const getTask = async () =>{
-            const res = await fetch('/api/auth/tasks')
-            const tasks = await res.json()
-    
-            setTasks(tasks)
-        }
-    
-        getTask()
+        fetchTasks()
+            .then((data) => setTasks(data))
+            .catch((error) => console.error('Error fetching tasks:', error))
+            .finally(() => setLoading(false))
     }, [])
 
     const handleShowModal = () => {
@@ -22,35 +21,30 @@ export function useTask(){
     }
     
     const handleSubmit = async (taskForm: Task) => {
-        const method = taskForm.id != 0 ? 'PUT' : 'POST'
-        const taskData = taskForm
-    
-        const res = await fetch('/api/auth/tasks', {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(taskData),
-        })
-        
-        if (!res.ok) return
-    
+
         setTaskEdit(null)
     
-        const data = await res.json()
-    
-        if (method === 'PUT'){
-            const updatedTasks= tasks.map(task => task.id === taskForm.id ? taskForm : task)
+        if (taskForm.Id !== 0){
+            await updateTask(taskForm)
+            
+            toast.success('Task updated Successfully!')
+
+            const updatedTasks= tasks.map(task => task.Id === taskForm.Id ? taskForm : task)
             setTasks(updatedTasks)
         }
         else {
+            const data = await addTask(taskForm)
+            
+            toast.success('Task added Successfully!')
+
             const updatedTasks = [...tasks, data]
             setTasks(updatedTasks)
         }
     }
     
     const handleEdit = (id: number) => {
-        const task = tasks.find(task => task.id === id)
+        const task = tasks.find(task => task.Id === id)
+        
         if (task) {
             setTaskEdit(task)
             setShowModal(true)
@@ -58,19 +52,15 @@ export function useTask(){
     }
     
     const handleDelete = async (id: number) => {
-        fetch('/api/auth/tasks', {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({id: id}),
-        })
+        await deleteTask(id)
+        
+        toast.success('Task deleted Successfully!')
     
         setTimeout(() => {
-            const updatedTasks = tasks.filter(task => task.id!== id)
+            const updatedTasks = tasks.filter(task => task.Id!== id)
             setTasks(updatedTasks)
         }, 1000)
     }
 
-    return {tasks, handleDelete, handleEdit, handleSubmit, handleShowModal, showModal, taskEdit, setTaskEdit, setShowModal}
+    return {tasks, handleDelete, handleEdit, handleSubmit, handleShowModal, showModal, taskEdit, setTaskEdit, setShowModal, loading}
 }
