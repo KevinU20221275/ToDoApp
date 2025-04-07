fb0c2c0ecc66c2fced9603f20ef90c7d8af7bd4a
KevinU20221275 <U20221275@univo.edu.sv>
2025-04-07 12:03:22 -0600

    add implementation for Toaster notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
+        <Toaster position="bottom-right" reverseOrder={false} />
         <div className="w-full min-h-screen flex justify-center items-center bg-[#eee] transition-all duration-700 dark:bg-[#444]">
           {children}
         </div>
